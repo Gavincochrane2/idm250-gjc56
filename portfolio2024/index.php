3c3f612495cebc966cdf735a412453e064e41502
Gavin Cochrane <gjc56@drexel.edu>
2024-03-03 16:55:15 -0500

    index.php update

--- portfolio2024/index.php
+++ portfolio2024/index.php
@@ -1,11 +1,21 @@
 
-    <?php get_header(); ?>
-    <h1>
-        <?php
+<?php get_header(); ?>
 
-echo get_the_title();
 
-        ?>
-        Home
-    </h1>
-    <?php get_footer(); ?>
+<div class="content">
+<?php
+echo get_the_content();
+?>
+</div>
+<?php 
+$projects = new WP_Query([
+   'post_type' => 'projects',
+   'posts_per_page' => 3,
+   'orderby' => 'date',
+   'order' => 'DESC'
+]);
+
+?>
+<?php get_footer(); ?>
+
+</html>
